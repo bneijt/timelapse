dbb64b5792ce95774955144fb4dc99b45947d4fc
A. Bram Neijt <bneijt@gmail.com>
2013-08-07 15:01:07 +0200

    Initial set up, no multiple select yet

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,27 @@ module Main where
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
 
+addFileToSelectionList window = do
+    dialog <- fileChooserDialogNew Nothing (Just window) FileChooserActionOpen [("Cancel", ResponseCancel), ("Add", ResponseAccept)]
+    widgetShow dialog
+    response <- dialogRun dialog
+    case response of
+          ResponseCancel -> putStrLn "You cancelled..."
+          ResponseAccept -> do nwf <- fileChooserGetFilename dialog
+                               case nwf of
+                                    Nothing -> putStrLn "Nothing"
+                                    Just path -> putStrLn ("New file path is:\n" ++ path)
+    widgetDestroy dialog
+    return ()
+
+main :: IO()
 main = do
     initGUI
     builder <- builderNew
     builderAddFromFile builder "main.glade"
+    addFileButton <- builderGetObject builder castToButton "addFileButton"
     mainWindow <- builderGetObject builder castToWindow "mainWindow"
-    --on mainWindow objectDestroy mainQuit
+    onClicked addFileButton (addFileToSelectionList mainWindow)
+    on mainWindow objectDestroy mainQuit
     widgetShowAll mainWindow
     mainGUI
